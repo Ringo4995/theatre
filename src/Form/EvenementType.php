3fa864ebb10c39972b058aa587196bdec775d1a9
Vincent <will.beyaert@gmail.com>
2024-02-01 12:50:44 +0100

    ajout de creation d'event & crud

---
+++ src/Form/EvenementType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evenement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('image')
+            ->add('lieu')
+            ->add('description')
+            ->add('prix')
+            ->add('dateEvenement')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+}
